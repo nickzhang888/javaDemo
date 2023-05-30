19b523a0e18eb531efcb79dc1fc0c0cdb6ee899d
zhangyulong <1601044331@qq.com>
2023-05-30 14:03:34 +0800

    优化

--- StrBuffer/SBuffer.java
+++ StrBuffer/SBuffer.java
@@ -3,6 +3,8 @@ package StrBuffer;
 import Modifier.Father;
 
 public class SBuffer extends Father {
+    // 注意String是引用类型
+    // 被final修饰的变量不可以被改变，被final修饰不可变的是变量的引用，而不是引用指向的内容，引用指向的内容是可以改变的
     public final String str = "comeIn";
 
     public final void myBuffer() {
@@ -11,6 +13,7 @@ public class SBuffer extends Father {
 
     public static void main(String[] args) {
         String str = "hello";
+        System.out.println(str);
         // 只能使用StringBuffer的构造方法创建
         StringBuffer sb = new StringBuffer();
         // 我们使用length()方法获取了字符串"hello"的长度
